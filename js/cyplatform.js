be859ddef78d47065f2358d4732ba70caf1ac6f0
ccvv <espace@aliyun.com>
2016-08-25 14:16:38 +0800

    fix

--- js/cyplatform.js
+++ js/cyplatform.js
@@ -1,23 +1,21 @@
 // 手机访问还是电脑访问
-var platform = function(){
-	var p = navigator.platform;
-	return p;
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
 }
 
-
-alert(platform());
-
-var isMovie = false;
-var pcs = ['win','mac','xll'];
-
-for(var i = 0; i < pcs.length; i++){
-	if(navigator.platform.indexOf(pcs[i])!=-1){
-		isMovie = true;
-	}	
-}
-
-if(isMovie){
-	window.location.href = 'movie/index.html';
-}else{
+if(IsPC()){
 	window.location.href = 'pc/index.html';
+}else{
+	window.location.href = 'movie/index.html';
 }
