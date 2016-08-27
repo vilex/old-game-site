b37fe8470d43e838c1a787463ff16440b2ea9dcb
ccvv <espace@aliyun.com>
2016-08-27 15:53:56 +0800

    fix

---
+++ skins/g-sm/base.php
@@ -0,0 +1,3 @@
+<!--
+    document.write('<script>var admPingUrl="http://eezdx.erc.18183.com/ping.gif?t?=1?t?=31,273,0,0|31,274,0,0|31,272,0,0?t?=31?t?=1472267691?t?=24058886611940597074";newhanawa.init();</script>');
+    -->
---
+++ skins/g-sm/js.php
@@ -0,0 +1,4 @@
+<!--
+var customadmurl = "http://eezdx.erc.18183.com/base.php?pid=31&time=1472267691&codeid="+newhanawa.codeid;
+document.write('<script type="text/javascript" src="'+customadmurl+'"></script>');
+-->
