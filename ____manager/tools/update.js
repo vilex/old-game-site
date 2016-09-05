ab5cec5317f402f386b0f23fcb55cfc5d21ce8dc
ccvv <espace@aliyun.com>
2016-09-05 12:03:53 +0800

    fix

--- ____manager/tools/update.js
+++ ____manager/tools/update.js
@@ -3,7 +3,7 @@ var XLSX = require('xlsx');
 var fs = require('fs');
 
 
-var workbook = XLSX.readFile('docs/game.xlsx',{type:'binary'});
+var workbook = XLSX.readFile('../docs/game.xlsx',{type:'binary'});
 
 
 
@@ -21,13 +21,27 @@ for(var i = 2; i < 10; i++){
 }
 
 bjtj_obj = 'var bjtj = ' + JSON.stringify(bjtj_obj);
-write('../config/bjtj.js',bjtj_obj);
+write('../../config/bjtj.js',bjtj_obj);
 
+// 更多小游戏
+var gdxyx = workbook.Sheets['更多小游戏'];
+var gdxyx_obj = {};
+for(var i = 2; i < 17; i++){
+    var key = gdxyx['A'+i]['w'];
+    gdxyx_obj[key] = {};
+    gdxyx_obj[key].name = gdxyx['B'+i]['w'];
+    gdxyx_obj[key].type = gdxyx['C'+i]['w'];
+    gdxyx_obj[key].des = gdxyx['D'+i]['w'];
+    gdxyx_obj[key].icon = gdxyx['E'+i]['w'];
+}
 
+gdxyx_obj = 'var gdxyx = ' + JSON.stringify(gdxyx_obj);
+write('../../config/gdxyx.js',gdxyx_obj);
 
 
 
 // 写文件
 function write(name,data){
     fs.writeFileSync(name,data);
+    console.log('output == > ' , name );
 }
