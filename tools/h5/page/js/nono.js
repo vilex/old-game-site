95254560c6edadac23c0cb8f0a38020c2ba008b8
ccvv <ilovecoding@yeah.net>
2016-09-24 18:04:04 +0800

    tool

---
+++ tools/h5/page/js/nono.js
@@ -0,0 +1,23 @@
+//进入全屏
+function requestFullScreen() {
+    var element = document.documentElement;
+    if(element.requestFullscreen) {
+	    element.requestFullscreen();
+	  } else if(element.mozRequestFullScreen) {
+	    element.mozRequestFullScreen();
+	  } else if(element.webkitRequestFullscreen) {
+	    element.webkitRequestFullscreen();
+	  } else if(element.msRequestFullscreen) {
+	    element.msRequestFullscreen();
+	  }
+}
+//退出全屏
+function exitFullscreen() {
+    if(document.exitFullscreen) {
+	    document.exitFullscreen();
+	  } else if(document.mozCancelFullScreen) {
+	    document.mozCancelFullScreen();
+	  } else if(document.webkitExitFullscreen) {
+	    document.webkitExitFullscreen();
+	  }
+}
